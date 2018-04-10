f18d9a84f7b3247868fd5d3efc420a70a3912e24
leevileh <lehtonen.leevi@gmail.com>
2018-04-10 20:55:33 +0300

    Radan piirtäminen ja ajaminen toimii, joskin huonosti. Testiluokat ja ensimmäinen testi luotu. Jacoco käytössä

---
+++ CarGame/src/test/java/carGame/domain/CarTest.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carGame.domain;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import carGame.domain.Track;
+
+/**
+ *
+ * @author leevileh
+ */
+public class CarTest {
+    
+    public CarTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+
+//    @Test
+//    public void decelerationWorks() {
+//        Car testCar = new Car(10, 10, Track track = new Track (200, 200));
+//    }
+}
---
+++ CarGame/src/test/java/carGame/domain/TrackTest.java
@@ -0,0 +1,38 @@
+
+package carGame.domain;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class TrackTest {
+    
+    public TrackTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void trackMapContainsEmptyMaterial() {
+        Track testTrack = new Track(10,10);
+        assertEquals(TrackMaterial.EMPTY, testTrack.content(5, 5));
+    }
+}
